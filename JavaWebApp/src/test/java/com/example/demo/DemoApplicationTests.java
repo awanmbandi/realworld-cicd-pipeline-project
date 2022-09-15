cea6c6fec92830a153e53c5813e7c18a7aab5c64
Mbandi <mbandiaak@gmail.com>
2022-09-15 15:21:49 -0400

    deployed test/cases

--- JavaWebApp/src/test/java/com/example/demo/DemoApplicationTests.java
+++ JavaWebApp/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,43 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Before
-    public void setup() {
-        app = new Application();
-    }
-    
-    
-    @Test
-    public void testWordCountString() {
-        int count = app.countWords("this is a test");
-        assertTrue(count == 4);
-    }
-
-    @Test
-    public void testWordCountEmpty() {
-        int count = app.countWords("");
-        assertTrue(count == 0);
-    }
-
-    @Test
-    public void testWordCountNull() {
-        int count = app.countWords(null);
-        assertTrue(count == 0);
-    }
-
-    @Test
-    @Ignore
-    public void testWordCountMore() {
-        int count = app.countWords("this will be skipped");
-        assertTrue(count == 3);
-    }
-
-    @Test
-    public void testWordCountSingle() {
-        int count = app.countWords("test");
-        assertTrue(count == 1);
-    }
-
 	@Test
 	void contextLoads() {
 	}
