2c159dbfe3449d1f5ffcb41e903aa227b0c1be3c
Mbandi <mbandiaak@gmail.com>
2023-04-23 11:42:03 -0400

    updated JDK installation

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -12,6 +12,15 @@ sudo systemctl start jenkins
 sudo yum install git -y
 ###
 
+# Apache Maven Installation/Config
+#sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+#sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+#sudo yum install -y apache-maven
+#sudo yum install java-1.8.0-devel
+
+#sudo /usr/sbin/alternatives --config java
+#sudo /usr/sbin/alternatives --config javac
+
 # Use The Amazon Linux 2 AMI When Launching The Jenkins VM/EC2 Instance
 # Instance Type: t2.medium or small minimum
 # Open Port (Security Group): 8080 
