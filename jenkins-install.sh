dc5e288ce3869555478b6ff05f4b9591e8d0fa78
Mbandi <mbandiaak@gmail.com>
2023-04-23 10:46:59 -0400

    updated jenkins installation

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo amazon-linux-extras install epel -y
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
