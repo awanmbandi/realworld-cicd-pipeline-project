e976005453ca87d5974b331c543af5e39dec5a46
awanmbandi <68437700+awanmbandi@users.noreply.github.com>
2023-04-27 12:44:25 -0400

    added tomcat and ansible config

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo amazon-linux-extras install epel -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+sudo su
+yum update –y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+amazon-linux-extras install epel -y
+amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+systemctl enable jenkins
+systemctl start jenkins
 
 # Installing Git
-sudo yum install git -y
-###
+yum install git -y
+
+# Tomcat Server Installation
+sudo su
+amazon-linux-extras install tomcat8.5 -y
+systemctl enable tomcat
+systemctl start tomcat
+
+# Provisioning Ansible Deployer Access
+useradd ansibleadmin
+echo ansibleadmin | passwd ansibleadmin --stdin
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+echo "ansadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Apache Maven Installation/Config
 #sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
