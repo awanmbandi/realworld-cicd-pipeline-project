40b3ff2d60cbf1e72bb8927f3e93e020f21e021a
awanmbandi <68437700+awanmbandi@users.noreply.github.com>
2023-04-27 16:15:55 -0400

    Update tomcat-ssh-configure.sh

--- tomcat-ssh-configure.sh
+++ tomcat-ssh-configure.sh
@@ -11,3 +11,20 @@ echo ansibleadmin | passwd ansibleadmin --stdin
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 echo "ansadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+: <<'END'
+# Tomcat Server Installation
+sudo su
+amazon-linux-extras install tomcat8.5 -y
+systemctl enable tomcat
+systemctl start tomcat
+
+# Provisioning Ansible Deployer Access
+useradd ansibleadmin
+echo ansibleadmin | passwd ansibleadmin --stdin
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+echo "ansadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+END
+
+
