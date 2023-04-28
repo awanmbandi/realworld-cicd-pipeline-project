8dd2a000f765e44d6bcd5063e838d03971d643ab
Mbandi <mbandiaak@gmail.com>
2023-04-28 18:17:36 -0400

    added splunk config

--- archive/deploy-env-config.sh
+++ archive/deploy-env-config.sh
@@ -4,3 +4,5 @@ useradd ansibleadmin
 echo ansibleadmin | passwd ansibleadmin --stdin
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
+
+
