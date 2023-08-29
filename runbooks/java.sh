374f6fe79245ece1aba0599e68520e682e692510
awanmbandi <awanmbandi@gmail.com>
2023-08-29 16:25:10 -0400

    runbooks/java.sh

--- runbooks/java.sh
+++ runbooks/java.sh
@@ -2,3 +2,4 @@ export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
 export PATH=$PATH:$JAVA_HOME/bin
 export JRE_HOME=/usr/lib/jvm/jre
 export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+
