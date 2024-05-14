8793d2150d909a7b8a90159ee3404167d37adadf
Mbandi Awan <68437700+awanmbandi@users.noreply.github.com>
2024-05-13 20:37:20 -0400

    Create maven-install.sh

---
+++ maven-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+sudo yum install java-1.8.0-devel -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install git -y
