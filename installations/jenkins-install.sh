7fa731d13d3a9eed75587b48329e232b5d3935cc
awanmbandi <awanmbandi@gmail.com>
2024-05-20 18:19:07 -0400

    updated jenkins config

--- installations/jenkins-install.sh
+++ installations/jenkins-install.sh
@@ -19,7 +19,7 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinu
 sudo yum install terraform -y
 terraform --version
 
-# Installing NPM
+# Installing NPM and Snyk
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 15.0.0
