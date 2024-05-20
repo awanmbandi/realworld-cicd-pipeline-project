b7001f95410fa2f89a1addde289de7f43d2be55a
awanmbandi <awanmbandi@gmail.com>
2024-05-20 17:25:15 -0400

    terraform configs

--- terraform-cicd-pipeline-project-3/main.tf
+++ terraform-cicd-pipeline-project-3/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web_server" {
   subnet_id              = var.subnet_id
   key_name               = var.key_name
   user_data              = var.user_data
-  vpc_security_group_ids = [aws_security_group.vpc_security_group_ids.id]
+  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   depends_on             = [aws_security_group.web_server_sg]
   tags = {
     Name = "web_server"
