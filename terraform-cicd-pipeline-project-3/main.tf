3ac040012f57504a2895182446279fc39f4338de
awanmbandi <awanmbandi@gmail.com>
2024-05-20 15:28:52 -0400

    updated jenkins project 3

--- terraform-cicd-pipeline-project-3/main.tf
+++ terraform-cicd-pipeline-project-3/main.tf
@@ -1,7 +1,6 @@
 # Resources = Bucket resource
 resource "aws_s3_bucket" "mbandi_dev_bucket" {
   bucket = "mbandi-dev-bucket-126543279054"
-  region = "us-west-1"
   tags = {
     Environment = "dev"
     CostCenter  = "cc590"
