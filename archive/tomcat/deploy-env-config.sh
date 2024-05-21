4782e9e18155fa85b0baaae9700b0bd3b805179a
mohammad <fakure1994@gmail.com>
2024-05-21 00:57:50 -0400

    asdfad

--- archive/tomcat/deploy-env-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo su
-useradd ansibleadmin
-echo ansibleadmin | passwd ansibleadmin --stdin
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-systemctl restart sshd
-
-
