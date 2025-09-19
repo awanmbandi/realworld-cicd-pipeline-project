b0e3df51fae7044d721d50c9d0a8c0e8f781bdb3
Mbandi Awan <68437700+awanmbandi@users.noreply.github.com>
2025-09-18 21:45:07 -0400

    added node exporter installation config

--- tomcat-ssh-configure.sh
+++ tomcat-ssh-configure.sh
@@ -90,3 +90,25 @@ sudo sed -i 's@^#\?PasswordAuthentication .*@PasswordAuthentication yes@' /etc/s
 sudo sed -i '/^PasswordAuthentication yes/a ChallengeResponseAuthentication yes' /etc/ssh/sshd_config
 sudo systemctl restart ssh
 
+# ----------------------------------------
+# Install and Configure Node Exporter
+# ----------------------------------------
+# Create node_exporter user
+sudo useradd --no-create-home node_exporter
+
+# Download and install Node Exporter
+cd /tmp
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter-1.0.1.linux-amd64*
+
+# Download service file directly from GitHub (instead of assuming it exists locally)
+sudo wget -O /etc/systemd/system/node-exporter.service \
+  https://raw.githubusercontent.com/awanmbandi/realworld-cicd-pipeline-project/refs/heads/prometheus-and-grafana-install/node-exporter.service
+
+# Reload systemd and enable/start Node Exporter
+sudo systemctl daemon-reload
+sudo systemctl enable node-exporter
+sudo systemctl start node-exporter
+
